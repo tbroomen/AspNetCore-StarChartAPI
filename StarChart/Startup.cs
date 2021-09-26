fad3a275d7392f91b0dd3ad888239dda082a6135
Trent Broome <tbroomen@gmail.com>
2021-09-26 21:13:26 +1000

    Update Startup.cs
    Tests failing??

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -12,7 +12,7 @@ namespace StarChart
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(); 
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("StarChart"));
         }
 
