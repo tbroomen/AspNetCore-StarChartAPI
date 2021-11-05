aa01e42f8a217f1737a665f2eb10a7c4431bd786
Trent Broome <tbroomen@gmail.com>
2021-11-05 16:01:26 +1100

    Part 5

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -18,5 +20,48 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.Where(x => x.Id == id).FirstOrDefault();
+
+            if (celestialObject is null)
+                return NotFound();
+
+            celestialObject.Satellites = GetRelatedCelestialObjects(celestialObject.Id);
+
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObjects = _context.CelestialObjects.Where(x => x.Name == name).ToList();
+
+            if (celestialObjects is null || !celestialObjects.Any())
+                return NotFound();
+
+            foreach (var obj in celestialObjects) 
+            {
+                obj.Satellites = GetRelatedCelestialObjects(obj.Id);
+            }
+            return Ok(celestialObjects);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = _context.CelestialObjects.ToList();
+
+            foreach(var obj in celestialObjects)
+            {
+                obj.Satellites = GetRelatedCelestialObjects(obj.Id);
+            }
+
+            return Ok(celestialObjects);
+        }
+
+        private List<CelestialObject> GetRelatedCelestialObjects(int id) => _context.CelestialObjects.Where(x => x.Id == id).ToList();
     }
 }
